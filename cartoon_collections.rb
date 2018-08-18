e8f407d5078d49c529833b18dcbc55e9e5149f23
Laura Darlak <laura@polyporedesign.com>
2018-08-18 17:50:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,11 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array)
-  array.all? do |call|
-    call.length > 3
+  array.none?{|call| call.length > 3}
+  if array.include?(false)
+    true
+  else false
   end
-  array.include?(false)
 end
 
 def find_the_cheese# code an argument here
