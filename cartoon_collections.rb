6c7fb10626641cee661c17f92d296a662fa51d87
Laura Darlak <laura@polyporedesign.com>
2018-08-18 17:59:08 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(array)
 end
 
 # iterate over each call and find the length
-# if the length is greater than 3 
+# if the length is greater than 3
 
 def find_the_cheese# code an argument here
   # the array below is here to help
