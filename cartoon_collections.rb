a739aaab8b2208cad8071fc5e158ec36939cd744
Laura Darlak <laura@polyporedesign.com>
2018-08-18 17:51:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array)
-  array.none?{|call| call.length > 3}
-  if array.include?(false)
+  array.any?{|call| call.length > 3}
+  if array.include?(true)
     true
   else false
   end
