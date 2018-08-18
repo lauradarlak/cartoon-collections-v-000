9cd945aa02adc5d6853bc8127757cd6af578d7ff
Laura Darlak <laura@polyporedesign.com>
2018-08-18 16:59:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(array)
-  # Your code here
+  array.each_with_index do |index, dwarf|
+    puts "#{index += 1} #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
